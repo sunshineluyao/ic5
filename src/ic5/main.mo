8447815c0b3a6a37df6715c14f852015b249efd1
duke-sunshine <lz183@duke.edu>
2022-03-21 02:24:20 +0800

    first commit

--- src/ic5/main.mo
+++ src/ic5/main.mo
@@ -1,5 +1,86 @@
+import Iter "mo:base/Iter";
+import List "mo:base/List";
+import Principal "mo:base/Principal";
+import Time "mo:base/Time";
+
 actor {
-    public func greet(name : Text) : async Text {
-        return "Hello, " # name # "!";
+    // public type Message = Text;
+    public type Message = {
+        text: Text;
+        time: Time.Time;
+        author: Text;
     };
+
+    public type Microblog = actor {
+        follow : shared (Principal) -> async ();
+        follows : shared query () -> async [Principal];
+        post : shared (Text) -> async ();
+        posts : shared query (since : Time.Time) -> async [Message];
+        timeline : shared () -> async [Message];
+        get_name : shared query () -> async ?Text;
+    };
+
+    stable var followed : List.List<Principal> = List.nil();
+    stable var messages : List.List<Message> = List.nil();
+    stable var _name : Text = "";
+
+    public shared func clean() : async () {
+        followed := List.nil();
+    };
+
+    public shared func set_name(name : Text) : async () {
+        _name := name;
+    };
+
+    public shared query func get_name() : async ?Text {
+        return ?_name;
+    };
+
+    public shared func follow(id : Principal) : async () {
+        followed := List.push(id, followed);
+    };
+
+    public shared query func follows() : async [Principal] {
+        return List.toArray(followed);
+    };
+
+    public shared func post(otp: Text, text: Text) : async () {
+        assert(otp == "123456");
+        let payload : Message = {
+            text = text;
+            time = Time.now();
+            author = _name;
+        };
+        messages := List.push(payload, messages);
+    };
+
+    public shared query func posts(since : Time.Time) : async [Message] {
+        var payload : List.List<Message> = List.nil();
+
+        for (msg in Iter.fromList(messages)) {
+            if (msg.time >= since) {
+                payload := List.push(msg, payload);
+            };
+        };
+
+        return List.toArray(payload);
+    };
+
+    public shared func timeline(since : Time.Time) : async [Message] {
+        var all : List.List<Message> = List.nil();
+
+        for (id in Iter.fromList(followed)) {
+            let canister : Microblog = actor(Principal.toText(id));
+            let msgs = await canister.posts(since);
+
+            for (msg in Iter.fromArray(msgs)) {
+                if (msg.time >= since) {
+                    all := List.push(msg, all);
+                };
+            };
+        };
+
+        return List.toArray(all);
+    };
+
 };
--- src/ic5_assets/src/index.js
+++ src/ic5_assets/src/index.js
@@ -1,19 +1,133 @@
-import { ic5 } from "../../declarations/ic5";
+import { ic5, idlFactory } from '../../declarations/ic5';
+import { Principal } from '@dfinity/principal';
+import { Actor, HttpAgent } from '@dfinity/agent';
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+var num_posts = -1;
+var num_followed = -1;
 
-  const name = document.getElementById("name").value.toString();
+let to_time = (target) => {
+  const temp = Number(target) / 1000000;
+  return new Date(temp).toLocaleString();
+};
 
-  button.setAttribute("disabled", true);
+let nameClicked = async (e) => {
+  let principal = e.target.id;
+  let agent = new HttpAgent();
+  let target_agent = Actor.createActor(idlFactory, {
+    agent: agent,
+    canisterId: Principal.fromText(principal),
+  });
+  let posts = await target_agent.posts(0);
+  let name = await target_agent.get_name();
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await ic5.greet(name);
+  console.log(principal);
 
-  button.removeAttribute("disabled");
+  let section = document.getElementById('posts-selected');
+  section.replaceChildren([]);
 
-  document.getElementById("greeting").innerText = greeting;
+  for (let i = 0; i < posts.length; i++) {
+    let post = document.createElement('p');
+    post.innerText =
+      posts[i].text + `\n${to_time(posts[i].time)}\n` + `Author: ${name}`;
+    section.appendChild(post);
+  }
+};
 
-  return false;
-});
+let follow = async () => {
+  document.getElementById('principal-error').innerText = '';
+  let follow_area = document.getElementById('follow');
+  let principal_txt = follow_area.value;
+  try {
+    let principal = Principal.fromText(principal_txt);
+    ic5.follow(principal);
+  } catch (error) {
+    console.error('wrong principal format');
+    document.getElementById('principal-error').innerText =
+      'wrong principal format.';
+  }
+};
+
+let load_followed = async () => {
+  let followed = await ic5.follows();
+  let followed_text = new Set();
+  let table = document.getElementById('followed');
+
+  if (followed.length === num_followed) return;
+  num_followed = followed.length;
+
+  table.replaceChildren([]);
+
+  table.innerHTML = '<tr><th>Name</th><th>CanisterID</th></tr>';
+
+  followed.forEach((e) => {
+    followed_text.add(e.toText());
+  });
+  let followed_arr = Array.from(followed_text);
+  for (let i = 0; i < followed_arr.length; i++) {
+    let agent = new HttpAgent();
+    let target_agent = Actor.createActor(idlFactory, {
+      agent: agent,
+      canisterId: Principal.fromText(followed_arr[i]),
+    });
+
+    try {
+      let name = await target_agent.get_name();
+
+      const table_item = `<tr><td><button id="${followed_arr[i]}">${name[0]}</button></td><td>${followed_arr[i]}</td></tr>`;
+      table.innerHTML += table_item;
+    } catch (error) {}
+  }
+
+  followed_arr.forEach((principal) => {
+    document.getElementById(principal).onclick = nameClicked;
+  });
+};
+
+let post = async () => {
+  document.getElementById('error').innerText = '';
+  let post_button = document.getElementById('post');
+  post_button.disabled = true;
+  let textarea = document.getElementById('message');
+  let text = textarea.value;
+  let otp = document.getElementById('otp').value;
+  try {
+    await ic5.post(otp, text);
+  } catch (error) {
+    console.error('wrong otp');
+    document.getElementById('error').innerText = 'Post Failed.';
+  }
+  post_button.disabled = false;
+};
+
+let load_posts = async () => {
+  let posts_section = document.getElementById('posts');
+  let posts = await ic5.posts(0);
+  let name = await ic5.get_name();
+
+  if (num_posts === posts.length) return;
+
+  posts_section.replaceChildren([]);
+
+  num_posts = posts.length;
+
+  for (let i = 0; i < posts.length; i++) {
+    let post = document.createElement('p');
+    post.innerText =
+      posts[i].text + `\n${to_time(posts[i].time)}\n` + `Author: ${name}`;
+    posts_section.appendChild(post);
+  }
+};
+
+let load = () => {
+  let post_button = document.getElementById('post');
+  let follow_button = document.getElementById('follow-btn');
+  post_button.onclick = post;
+  follow_button.onclick = follow;
+  load_posts();
+  load_followed();
+
+  setInterval(load_followed, 3000);
+  setInterval(load_posts, 3000);
+};
+
+window.onload = load;
